4a3303eb55ae22384b1b5e3ddaa2e7faa0f11399
Joel <rapp.joel20@gmail.com>
2024-07-24 20:22:06 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+function writeCards (names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++ ) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        messages.push(message)
+    }
+        return messages
+}
+
+function countdown(numbers) {
+    let n = 10
+    while (n > numbers.length) {
+        console.log(`${numbers}`)
+        n--
+    }
+    return numbers
+}
+
+function countDown() {
+    let number = 10
+    while (number >= 0) {
+        console.log(number--)
+    }
+    return number
+}
